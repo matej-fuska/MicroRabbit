d85e5997a56d9bc6afeb2bf710a76f22abdb97e4
Matej Fuska <matej.fuska@thermofisher.com>
2023-04-17 15:02:04 +0200

    CreateCommand and AccountTransfer model created

--- MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -4,23 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 
 namespace MicroRabbit.Banking.Application.Services
 {
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _eventBus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus eventBus)
         {
             _accountRepository = accountRepository;
+            _eventBus = eventBus;
         }
 
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                    accountTransfer.FromAccount,
+                    accountTransfer.ToAccount,
+                    accountTransfer.TransferAmount
+                );
+
+            _eventBus.SendCommand(createTransferCommand);
         }
     }
 }
